dddf29d2c07a6b4aae84481ca5c9e86d85326960
Dmdiws <ilyanorin1990@gmail.com>
2026-02-16 19:01:50 +1000

    Pre-final version of site

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ box.addEventListener('click', () => {
     if (!hasFallen) {
         hasFallen = 1;
         box.classList.remove('relax');
-        box.style.backgroundImage = "url('images/falling_fursone.PNG')";
+        box.style.backgroundImage = "url('Images/Paintings/Chibi/falling_fursone.PNG')";
         box.style.backgroundPositionY = "center";
         box.style.width = "13vw";
         box.style.height = "13vw";
         box.style.pointerEvents = "none";
         box.classList.toggle('is_clicked');
         box.addEventListener('animationend', function handler1() {
-            box.style.backgroundImage = "url('images/static_avatar.PNG')"
+            box.style.backgroundImage = "url('Images/Paintings/Chibi/static_avatar.PNG')"
             document.getElementById('fursone').classList.remove('is_clicked');
             box.style.pointerEvents = "auto";
             void box.offsetWidth;
@@ -38,3 +38,33 @@ box.addEventListener('click', () => {
         });
     }
 });
+
+const container = document.querySelector(".container"),
+    images = document.querySelectorAll(".big_topic"),
+    buttonL = document.querySelector("#left_button"),
+    buttonR = document.querySelector("#right_button")
+    carousel = document.querySelector(".carousel");
+
+let ImageIndex = 0, intervalID;
+const autoSlide = () => {
+    clearInterval(intervalID);
+    intervalID = setInterval(() => slideImage(++ImageIndex),2000);
+}
+autoSlide();
+const slideImage = () => {
+    ImageIndex = ImageIndex === images.length ? 0: ImageIndex < 0 ? images.length -1 : ImageIndex;
+    carousel.style.transform = `translate(-${ImageIndex*88}vw)`;
+};
+
+container.addEventListener("mouseover", () => clearInterval(intervalID));
+container.addEventListener("mouseleave",autoSlide);
+
+const updateClick = (e) => {
+    clearInterval(intervalID);
+    ImageIndex+=e.target.id === "right_button" ? 1 : -1;
+    slideImage(ImageIndex);
+    autoSlide();
+}
+
+buttonL.addEventListener("click",updateClick);
+buttonR.addEventListener("click",updateClick);
